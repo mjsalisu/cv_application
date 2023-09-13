b524e32dc15f91bb6429e6d032ae6b476b5c226d
Jamilu Salisu <jamilusalis@gmail.com>
2023-09-13 09:30:10 +0100

    user model

---
+++ lib/model/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String? name;
+  final String? slackUsername;
+  final String? gitHubUsername;
+  final String? bio;
+
+  UserModel({
+    this.name,
+    this.slackUsername,
+    this.gitHubUsername,
+    this.bio,
+  });
+}
